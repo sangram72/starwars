17cb03cbbf7ead6646e0999acfc37053fe014a11
Sangram Nandi <67709894+sangram72@users.noreply.github.com>
2024-05-23 15:30:55 +0530

    Update page.js

--- src/app/Character/page.js
+++ src/app/Character/page.js
@@ -1,61 +1,61 @@
 
 "use client"
-import { useEffect, useState ,Suspense} from 'react';
-import { Container, VStack, Center, Spinner } from '@chakra-ui/react';
-import CharacterDetail from '../../Components/CharacterDetail'; 
-import { useSearchParams } from 'next/navigation';
-// import {Suspense} from "react";
-const characterImages = [
-  '/luke.jpeg',
-  '/c3.jpg',
-  '/R22.jpg',
-  '/death.jpg',
-  '/leia.jpeg',
-  '/owen.jpg',
-  '/beru.jpg',
-  '/R5.jpg',
-  '/biggs.jpeg',
-  '/obi.jpg'
-];
+import { useEffect, useState } from 'react';
+import { Container,Spinner } from '@chakra-ui/react';
+import CharacterDetail from '../../Components/CharacterDetail';
 
-export default function CharacterPage() {
-  const searchParams = useSearchParams();
-  const id = searchParams.get('id');
+const CharacterPage = () => {
   const [character, setCharacter] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+
+  const characterImages = [
+    '/luke.jpeg',
+    '/c3.jpg',
+    '/R22.jpg',
+    '/death.jpg',
+    '/leia.jpeg',
+    '/owen.jpg',
+    '/beru.jpg',
+    '/R5.jpg',
+    '/biggs.jpeg',
+    '/obi.jpg'
+  ];
 
   useEffect(() => {
     const fetchCharacter = async () => {
-      try {
-        const response = await fetch(`https://swapi.dev/api/people/${id}/`);
-        const data = await response.json();
-        const films = await Promise.all(data.films.map(async (filmUrl) => {
-          const filmResponse = await fetch(filmUrl);
-          const filmData = await filmResponse.json();
-          return filmData.title;
-        }));
-        const characterImage = id <= 10 ? characterImages[id - 1] : null;
-        setCharacter({ ...data, films, image: characterImage });
-      } catch (error) {
-        console.error('Error fetching character details:', error);
+      const query = new URLSearchParams(window.location.search);
+      const id = query.get('id');
+
+      if (id) {
+        try {
+          const response = await fetch(`https://swapi.dev/api/people/${id}/`);
+          const data = await response.json();
+          const films = await Promise.all(
+            data.films.map(async (filmUrl) => {
+              const filmResponse = await fetch(filmUrl);
+              const filmData = await filmResponse.json();
+              return filmData.title;
+            })
+          );
+          const characterImage = id <= 10 ? characterImages[id - 1] : null;
+          setCharacter({ ...data, films });
+        } catch (error) {
+          console.error('Error fetching character details:', error);
+        } finally {
+          setLoading(false);
+        }
       }
     };
 
-    if (id) {
-      fetchCharacter();
-    }
-  }, [id]);
+    fetchCharacter();
+  }, []);
 
   return (
-    <Container maxW="container.md" py={4} centerContent>
-        <Suspense>
-      {character ? (
-        <CharacterDetail character={character} />
-      ) : (
-        <Center minH="100vh">
-          <Spinner size="xl" />
-        </Center>
-      )}
-      </Suspense>
+    <Container maxW="container.md" py={4}>
+      {loading ? <Spinner size="xl" /> : <CharacterDetail character={character} />}
     </Container>
   );
-}
+};
+
+export default CharacterPage;
