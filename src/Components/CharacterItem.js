0e091ed651353eccfb36d9f823ed2756f67e48b5
Sangram Nandi <67709894+sangram72@users.noreply.github.com>
2024-05-23 15:23:30 +0530

    Update CharacterItem.js

--- src/Components/CharacterItem.js
+++ src/Components/CharacterItem.js
@@ -1,50 +1,47 @@
 
-
 "use client"
-
-import { Box, Button, Text, Link as ChakraLink ,Image} from '@chakra-ui/react';
+import { Box, Button, Text, Link as ChakraLink, Image, Flex, Spacer } from '@chakra-ui/react';
 import NextLink from 'next/link';
-import { WarningIcon,StarIcon } from '@chakra-ui/icons';
+import { WarningIcon, StarIcon } from '@chakra-ui/icons';
+
 
 const CharacterItem = ({ character, toggleFavorite, favorites }) => {
+  const isFavorite = favorites.some((fav) => fav.name === character.name);
+
   return (
     <Box
-    p={4}
-    borderWidth={1}
-    bg="white"
-    borderRadius="lg"
-    width={{ base: '100%', sm: 'calc(50% - 8px)', md: 'calc(33.33% - 12px)', lg: 'calc(25% - 12px)' }}
-    minWidth="300px"
-    boxShadow="md"
-    _hover={{ bg: 'teal.100' }}
-    mb={{ base: 4, lg: 4 }}
-    mr={{ base: 0, sm: 4, md: 4, lg: 4 }}
+      p={4}
+      borderWidth={1}
+      bg="white"
+      borderRadius="lg"
+      color="black"
+      width={{ base: '100%', sm: 'calc(50% - 8px)', md: 'calc(33.33% - 12px)', lg: 'calc(25% - 12px)' }}
+      minWidth="300px"
+      boxShadow="md"
+      _hover={{ bg: 'teal.100' }}
+      mb={{ base: 4, lg: 4 }}
+      mr={{ base: 0, sm: 4, md: 4, lg: 4 }}
     >
-           <Image src="/starwars.jpg"  />
-      <NextLink href={`/Character?id=${character.id}`} >
+  {character.image && <Image src={character.image}  mb={4} height="170px" width="100%" borderRadius="10px"/>}
+      <NextLink href={`/Character?id=${character.id}`} passHref>
   
           <Text fontWeight="bold">{character.name}</Text>
           <Text>Height: {character.height} cm</Text>
           <Text>Mass: {character.mass} kg</Text>
- 
+
       </NextLink>
-      <Button
-size='sm'
-  colorScheme={favorites.find((fav) => fav.name === character.name)}
-  onClick={() => toggleFavorite(character)}
->
-  {favorites.find((fav) => fav.name === character.name) ? <WarningIcon w={4} h={4} color="red.500" /> : <StarIcon w={4} h={4} color="red.500" />}
-</Button>
+      <Flex mt={2}>
+        <Spacer />
+        <Button
+          size='sm'
+          colorScheme={isFavorite ? 'red' : 'blue'}
+          onClick={() => toggleFavorite(character)}
+        >
+          {isFavorite ? <WarningIcon w={4} h={4} /> : <StarIcon w={4} h={4} />}
+        </Button>
+      </Flex>
     </Box>
   );
 };
 
 export default CharacterItem;
-
-
-
-
-
-
-
-
