a4d53ee1ea3d206112b8db6dc430e9c99af7e0fc
Sangram Nandi <67709894+sangram72@users.noreply.github.com>
2024-05-23 15:15:30 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,46 @@
-// pages/index.js
+
 "use client"
-import { useState, useEffect } from 'react';
-import { Container, VStack, Spinner } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { Container, VStack, Button, Spinner, Flex, Box } from '@chakra-ui/react';
 import CharacterList from '../Components/CharacterList';
 import FavoriteCharacters from '../Components/FavoriteCharacters';
 import Navbar from '../Components/Navbar';
 
 export default function Home() {
+
+  const characterImages = [
+    '/luke.jpeg',
+    '/c3.jpg',
+    '/R22.jpg',
+    '/death.jpg',
+    '/leia.jpeg',
+    '/owen.jpg',
+    '/beru.jpg',
+    '/R5.jpg',
+    '/biggs.jpeg',
+    '/obi.jpg'
+  ];
   const [characters, setCharacters] = useState([]);
   const [loading, setLoading] = useState(true);
   const [favorites, setFavorites] = useState([]);
   const [showFavorites, setShowFavorites] = useState(false);
+  const [page, setPage] = useState('https://swapi.dev/api/people/');
+  const [nextPage, setNextPage] = useState(null);
+  const [prevPage, setPrevPage] = useState(null);
 
   useEffect(() => {
     const fetchCharacters = async () => {
+      setLoading(true);
       try {
-        const response = await fetch('https://swapi.dev/api/people/');
+        const response = await fetch(page);
         const data = await response.json();
-        setCharacters(data.results.map((character, index) => ({ ...character, id: index + 1 })));
+        setCharacters(data.results.map((character, index) => ({
+          ...character,
+          id: character.url.match(/\/([0-9]*)\/$/)[1], 
+          image: index < 10 ? characterImages[index] : null,
+        })));
+        // setNextPage(data.next);
+        // setPrevPage(data.previous);
       } catch (error) {
         console.error('Error fetching characters:', error);
       } finally {
@@ -26,46 +49,60 @@ export default function Home() {
     };
 
     fetchCharacters();
-  }, []);
+  }, [page]);
 
   useEffect(() => {
     const storedFavorites = JSON.parse(localStorage.getItem('favorites')) || [];
     setFavorites(storedFavorites);
   }, []);
 
-  useEffect(() => {
-    localStorage.setItem('favorites', JSON.stringify(favorites));
-  }, [favorites]);
-
   const toggleFavorite = (character) => {
     const index = favorites.findIndex((fav) => fav.name === character.name);
     if (index === -1) {
-      setFavorites([...favorites, character]);
+      const newFavorites = [...favorites, character];
+      setFavorites(newFavorites);
+      localStorage.setItem('favorites', JSON.stringify(newFavorites));
     } else {
       const updatedFavorites = [...favorites];
       updatedFavorites.splice(index, 1);
       setFavorites(updatedFavorites);
+      localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
     }
   };
 
+  const toggleFavoritesVisibility = () => {
+    setShowFavorites(!showFavorites);
+  };
+
+  const handleSearch = (searchTerm) => {
+    const filteredCharacters = characters.filter((character) =>
+      character.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setCharacters(filteredCharacters);
+  };
+
   return (
-    <>
-      <Navbar hasFavorites={favorites.length > 0} showFavorites={showFavorites} toggleFavoritesVisibility={() => setShowFavorites(!showFavorites)} />
-      <Container maxW="container.md" py={4} >
+    <Box
+     
+      bgSize="cover"
+      bgPosition="center"
+      minHeight="100vh"
+    >
+      <Navbar showFavorites={showFavorites} toggleFavoritesVisibility={toggleFavoritesVisibility} onSearch={handleSearch} />
+      <Container maxW="container.lg" py={4}>
         <VStack spacing={4}>
           {loading ? (
-            <Spinner size="xl" />
+            <Spinner size="xl" color='white'/>
+          ) : showFavorites ? (
+            <FavoriteCharacters favorites={favorites} toggleFavorite={toggleFavorite} />
           ) : (
             <>
-              {showFavorites ? (
-                <FavoriteCharacters favorites={favorites} toggleFavorite={toggleFavorite} />
-              ) : (
-                <CharacterList characters={characters} toggleFavorite={toggleFavorite} favorites={favorites} />
-              )}
+              <CharacterList characters={characters} toggleFavorite={toggleFavorite} favorites={favorites} />
+             
             </>
           )}
         </VStack>
       </Container>
-    </>
+    </Box>
   );
 }
