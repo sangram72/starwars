443ac44f86f1c66d59b49f9bb3b84e2dedf39904
Sangram Nandi <67709894+sangram72@users.noreply.github.com>
2024-05-23 15:15:54 +0530

    Update page.js

--- src/app/Character/page.js
+++ src/app/Character/page.js
@@ -1,46 +1,59 @@
 
 "use client"
 import { useEffect, useState } from 'react';
-import { Container,Spinner } from '@chakra-ui/react';
-import CharacterDetail from '../../Components/CharacterDetail';
+import { Container, VStack, Center, Spinner } from '@chakra-ui/react';
+import CharacterDetail from '../../Components/CharacterDetail'; 
+import { useSearchParams } from 'next/navigation';
 
-const CharacterPage = () => {
+const characterImages = [
+  '/luke.jpeg',
+  '/c3.jpg',
+  '/R22.jpg',
+  '/death.jpg',
+  '/leia.jpeg',
+  '/owen.jpg',
+  '/beru.jpg',
+  '/R5.jpg',
+  '/biggs.jpeg',
+  '/obi.jpg'
+];
+
+export default function CharacterPage() {
+  const searchParams = useSearchParams();
+  const id = searchParams.get('id');
   const [character, setCharacter] = useState(null);
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchCharacter = async () => {
-      const query = new URLSearchParams(window.location.search);
-      const id = query.get('id');
-
-      if (id) {
-        try {
-          const response = await fetch(`https://swapi.dev/api/people/${id}/`);
-          const data = await response.json();
-          const films = await Promise.all(
-            data.films.map(async (filmUrl) => {
-              const filmResponse = await fetch(filmUrl);
-              const filmData = await filmResponse.json();
-              return filmData.title;
-            })
-          );
-          setCharacter({ ...data, films });
-        } catch (error) {
-          console.error('Error fetching character details:', error);
-        } finally {
-          setLoading(false);
-        }
+      try {
+        const response = await fetch(`https://swapi.dev/api/people/${id}/`);
+        const data = await response.json();
+        const films = await Promise.all(data.films.map(async (filmUrl) => {
+          const filmResponse = await fetch(filmUrl);
+          const filmData = await filmResponse.json();
+          return filmData.title;
+        }));
+        const characterImage = id <= 10 ? characterImages[id - 1] : null;
+        setCharacter({ ...data, films, image: characterImage });
+      } catch (error) {
+        console.error('Error fetching character details:', error);
       }
     };
 
-    fetchCharacter();
-  }, []);
+    if (id) {
+      fetchCharacter();
+    }
+  }, [id]);
 
   return (
-    <Container maxW="container.md" py={4}>
-      {loading ? <Spinner size="xl" /> : <CharacterDetail character={character} />}
+    <Container maxW="container.md" py={4} centerContent>
+      {character ? (
+        <CharacterDetail character={character} />
+      ) : (
+        <Center minH="100vh">
+          <Spinner size="xl" />
+        </Center>
+      )}
     </Container>
   );
-};
-
-export default CharacterPage;
+}
